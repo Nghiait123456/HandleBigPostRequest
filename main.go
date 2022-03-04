2b1cba12ecc0c4e9008549c954b126703040c676
nghiapm <nghiapm@appota.com>
2022-03-04 16:06:43 +0700

    cmt DB for test EC2

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/kataras/iris/v12"
 	"handle-big-post-request/config"
-	"handle-big-post-request/connect/database"
 	"handle-big-post-request/controllers"
 	"handle-big-post-request/handle"
 	"handle-big-post-request/queue"
@@ -15,7 +14,8 @@ var poolWorkerUpload queue.PoolJob
 func init() {
 	poolWorkerUpload = queue.PoolJob{make(chan queue.Job, 200000), 80000}
 	config.Init("./config.yml")
-	database.Init()
+	// todo cmt for test on EC2
+	//database.Init()
 }
 
 func main() {
